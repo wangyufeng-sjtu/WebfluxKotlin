9dc29e32bc3e9556ae98ebd054e8590bed30ca48
wangyufeng-sjtu <wangyufeng@sjtu.edu.cn>
2020-08-02 18:48:33 +0800

    Controller with coroutine style

--- src/main/kotlin/com/wanger/example/webfluxkotlin/controller/StreamController.kt
+++ src/main/kotlin/com/wanger/example/webfluxkotlin/controller/StreamController.kt
@@ -1,6 +1,6 @@
 package com.wanger.example.webfluxkotlin.controller
 
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.reactor.mono
@@ -21,11 +21,22 @@ class StreamController{
 
     @RequestMapping("/printMultiContent",
             method = [RequestMethod.GET, RequestMethod.GET])
-    fun multipleElement(content:String): Flow<String> {
+    fun multipleElement(content:String): Flow<String>{
+        val scope = CoroutineScope(Dispatchers.IO)
+        val start = System.currentTimeMillis()
+        var jobList = ArrayList<Deferred<String>>()
         val result = flow{
             for(i in 1..3){
-                delay(1000L*i)
-                emit( "Received content: $content $i \n")
+                val temp = scope.async {
+                    delay(10000L + 2000L*i)
+                    "Received content: $content $i \n"
+                }
+                jobList.add(temp)
+            }
+            for(job in jobList){
+                emit(job.await())
+                val end = System.currentTimeMillis()
+                println("Spend ${start - end} ms")
             }
         }
         return result
